8e9e30204e04ad7b8f5fcef997aae9ebaf5d2ef3
Apoorv Mittal <mittal.apoorv2012@gmail.com>
2018-04-01 12:17:58 -0400

    resolved conflict in support.php

--- support.php
+++ support.php
@@ -24,13 +24,4 @@ EOPAGE;
     return $page;
 }
 
-function connectToDB($host, $user, $password, $database) {
-    $db = mysqli_connect($host, $user, $password, $database);
-    if (mysqli_connect_errno()) {
-        echo "Connect failed.\n".mysqli_connect_error();
-        exit();
-    }
-    return $db;
-}
-
 ?>
