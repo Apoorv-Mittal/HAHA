a994e0e4ec650e7389b99328123b5a19493fd5be
aliparacha1996 <35855650+aliparacha1996@users.noreply.github.com>
2018-04-01 12:08:41 -0400

    Add files via upload

--- support.php
+++ support.php
@@ -1,22 +1,35 @@
 <?php
 
-function generatePage($body, $title="Events") {
+function generatePage($body, $title="Example") {
     $page = <<<EOPAGE
 <!doctype html>
 <html>
-     <head> 
+    <head> 
         <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>$title</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" >
     </head>
             
     <body>
-    <div class="container-fluid">
+        <div class="container-fluid">
             $body
-    </div>
+        </div>
     </body>
 </html>
 EOPAGE;
-    echo $page;
+
+    return $page;
 }
+
+function connectToDB($host, $user, $password, $database) {
+    $db = mysqli_connect($host, $user, $password, $database);
+    if (mysqli_connect_errno()) {
+        echo "Connect failed.\n".mysqli_connect_error();
+        exit();
+    }
+    return $db;
+}
+
 ?>
