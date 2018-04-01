3b7e59a1c9670c28e84b49698e69240bd1ac3955
Apoorv Mittal <mittal.apoorv2012@gmail.com>
2018-04-01 16:05:52 -0400

    half unimplemented method

--- interests.php
+++ interests.php
@@ -2,3 +2,25 @@
 - then display the ones you haven't checked
 - create new category
 - return to user.php
+
+<?php
+
+require_once 'support.php';
+require_once 'db.php';
+
+session_start();
+$email= $_SESSION['email'];
+
+$result = queryForDB("select * from interests where email = ".$email);
+$num_rows = $result->num_rows;
+$row = $result->fetch_array(MYSQLI_ASSOC);
+$i=0;
+$body ="<table>";
+for ($i=0;$i<$num_rows;$i++){
+    $body.= "<tr><td>".$row['category']."</td><td><input type=\"submit\" name=\"".$row['category']."\" formaction=\"interests.php\" formmethod=\"post\"></td></tr>";
+}
+
+$body.="</table>";
+
+
+generatePage($body,"Your Categories");
