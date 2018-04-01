eec5e5dbfefafcda59937ccdb1e32eadcab6a0d9
Apoorv Mittal <mittal.apoorv2012@gmail.com>
2018-04-01 12:19:12 -0400

    resolved conflict in support.php

--- support.php
+++ support.php
@@ -3,11 +3,9 @@
 function generatePage($body, $title="Example") {
     $page = <<<EOPAGE
 <!doctype html>
-<!doctype html>
 <html>
     <head> 
         <meta charset="utf-8" />
-        
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>$title</title>
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" >
