1b6f657b40cdb9c60102606191a80141e3e33389
JeremyMBell <jeremybell@wireless-10-105-44-34.umd.edu>
2018-04-22 13:50:04 -0400

    Get events to weigh interest in

--- algo.php
+++ algo.php
@@ -52,3 +52,82 @@
 
 
     }
+
+    function algoForX() {// X is every event
+    	return queryForDB("select start_time, end_time, date, type, event_id from event_date");
+    }
+
+    function algoForY() {// Y is events the user is participating in
+    	return queryForDB("select p.event_id, ed.start_time, ed.end_time, ed.date, ed.type from participants p left join event_date e on p.event_id = e.event_id where p.email = '".$_SESSION["email"]."'");
+    }
+
+    function eventsOverlap($e1, $e2) {
+    	// Part 1: do the dates overlap?
+    	// if they're the same day of year, then the date will overlap.
+    	$sameDay = $e1["date"]["tm_yday"] === $e2["date"]["tm_yday"];
+
+    	// if one of the events recurs and they fall on the same weekday, the date will overlap.
+    	if (!$sameDay && ($e1["recurring"] || $e2["recurring"])) {
+    		$sameDay = $e1["date"]["tm_wday"] === $e2["date"]["tm_wday"];
+    	}
+
+    	if (!$sameDay) {
+    		return false;
+    	}
+
+    	// Part 2: do the times overlap?
+    	$e1Times = array($e1["start_time"]["tm_min"] + $e1["start_time"]["tm_hour"]*60, $e1["end_time"]["tm_min"] + $e1["end_time"]["tm_hour"]*60);
+    	$e2Times = array($e2["start_time"]["tm_min"] + $e2["start_time"]["tm_hour"]*60, $e2["end_time"]["tm_min"] + $e2["end_time"]["tm_hour"]*60);
+    	// 0 => start time, 1 => end time
+    	return ($e1Times[0] >= $e2Times[0] && $e1Times[0] <= $e2Times[1]) ||
+    		   ($e1Times[1] >= $e2Times[0] && $e1Times[1] <= $e2Times[1]);
+    }
+
+    function weighInterest() {
+    	// Returns events we should weigh interest in. Exclude events where the time overlaps or that the user is already participating.
+    	$TIME_FORMAT = "%H:%M:%S";
+    	$DATE_FORMAT = "%Y:%m:%d";
+		/*
+
+		TIME FORMAT:
+		HH:MM:SS
+		24-hour time
+		0-padded
+
+		DATE FORMAT:
+		YYYY-MM-DD
+		0-padded
+
+		*/
+    	$X = algoForX();
+    	$XArray = array();
+    	while ($x = $X->fetch_assoc()) {
+    		$parsedX = array(
+    			"event_id" => $x["event_id"],
+	    		"start_time" => strptime($x["start_time"], $TIME_FORMAT),
+	    		"end_time" => strptime($x["end_time"], $TIME_FORMAT),
+	    		"date" => strptime($x["date"], $DATE_FORMAT),
+	    		"recurring" => $x["type"] === "WEEKLY"
+	    	);
+    		$XArray[] = $parsedX;
+    	}
+    	$Y = algoForY();
+    	while($y = $Y->fetch_assoc()) {
+    		$parsedY = array(
+    			"event_id" => $y["event_id"],
+	    		"start_time" => strptime($y["start_time"], $TIME_FORMAT),
+	    		"end_time" => strptime($y["end_time"], $TIME_FORMAT),
+	    		"date" => strptime($y["date"], $DATE_FORMAT),
+	    		"recurring" => $y["type"] === "WEEKLY"
+    		);
+    		$overlapped = false;
+    		$tempX = array();
+    		foreach ($XArray as $x) {
+    			if (!eventsOverlap($x, $parsedY)) {
+    				$tempX[] = $x;
+    			}
+    		}
+    		$XArray = $tempX;
+    	}
+    	return $XArray;
+    }
