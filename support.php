8120e42a5a9c8b08626739361530a99ece5dda60
Apoorv Mittal <mittal.apoorv2012@gmail.com>
2018-04-22 12:36:50 -0400

    Added custom CSS functionality

--- support.php
+++ support.php
@@ -1,5 +1,5 @@
 <?php
-function generatePage($body, $title="Events") {
+function generatePage($body, $title="Events", $css="") {
     $errorLogs = "";
     if (function_exists("getErrors")) {
         $errors = getErrors();
@@ -14,7 +14,8 @@ function generatePage($body, $title="Events") {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>$title</title>
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" >
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" >
+        $css
     </head>
             
     <body>
