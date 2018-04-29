f0dec8195ec1765ea9a1aea8ba66ae19463a0823
Apoorv Mittal <mittal.apoorv2012@gmail.com>
2018-04-29 16:40:32 -0400

    Removed the error which comes when you attend an event

--- data.php
+++ data.php
@@ -21,14 +21,11 @@ mysqli_select_db($res, "events");
 $scheduler = new schedulerConnector($res, "MySQLi");
 
 $query =
-    "select events.*
+    "select events.event_id, events.start_date, events.end_date, events.type, events.owner_email, events.title, events.description, events.category
     from events, participants
     where events.owner_email='{$email}' or (events.event_id=participants.event_id and participants.email='{$email}')";
 
 if ($scheduler->is_select_mode())
     //code for loading data
     $scheduler->render_sql($query,"event_id","start_date,end_date,title,type,owner_email,description,image");
-else
-    //code for other operations - i.e. update/insert/delete
-    $scheduler->render_table("events","event_id","start_date,end_date,title,type,owner_email,description,image");
 ?>
